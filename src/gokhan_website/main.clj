13ba0abfd4af18c38731f47f45ad086afe994f1b
gokhandilek <gokhanengineering@gmail.com>
2020-05-05 23:41:56 +0800

    deleted commented out code

--- src/gokhan_website/main.clj
+++ src/gokhan_website/main.clj
@@ -10,6 +10,3 @@
     (run-server (app) {:port port})))
 ;
 ;
-;(defn -main [& [port]]
-;  (let [port (Integer. (or port (env :port) 5000))]
-;    (jetty/run-jetty (site #'app) {:port port :join? false})))
